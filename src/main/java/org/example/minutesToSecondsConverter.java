67169bc8b266b12e790f8f12fb67fb859c991859
Sorin Cristescu <sorinandrei.cristescu@yahoo.com>
2024-05-14 11:25:43 +0300

    Implement minutes to second converter

---
+++ src/main/java/org/example/minutesToSecondsConverter.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class minutesToSecondsConverter {
+    int minutes = 5;
+    int seconds = 60;
+    public void secondsToMinutes() {
+        System.out.println("5 minutes equals " + minutes*seconds + " seconds.");
+    }
+}
